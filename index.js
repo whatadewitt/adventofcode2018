fbededc7301c8fae109fa82a703ae22f4a1feaa6
Luke DeWitt <luke.dewitt@redspace.com>
2018-12-03 22:48:07 -0400

    d3p2

--- index.js
+++ index.js
@@ -12,9 +12,7 @@ function readInput() {
   const row = [];
   row.length = 1000;
 
-  let i,
-    j,
-    doubled = 0;
+  let i, j;
   for (i = 0; i < fabric.length; i++) {
     fabric[i] = [...row];
   }
@@ -28,11 +26,12 @@ function readInput() {
       top = top.substr(0, top.length - 1);
       let [width, height] = size.split("x");
       claims.push({
-        id,
+        id: parseInt(id.substr(1), 10),
         top: parseInt(top, 10),
         left: parseInt(left, 10),
         width: parseInt(width, 10),
-        height: parseInt(height, 10)
+        height: parseInt(height, 10),
+        winner: true
       });
     })
     .on("close", () => {
@@ -43,14 +42,16 @@ function readInput() {
               fabric[i][j] = id;
             } else {
               if ("X" !== fabric[i][j]) {
-                doubled++;
+                claims[fabric[i][j] - 1].winner = false;
               }
+
+              claims[id - 1].winner = false;
               fabric[i][j] = "X";
             }
           }
         }
       });
-      console.log(doubled);
+      console.log(claims.filter(c => c.winner));
 
       process.exit(0);
     });
