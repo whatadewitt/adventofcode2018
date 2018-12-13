32e509486781201fa8c0954db6c4bcee9e957ae5
Luke DeWitt <luke.dewitt@redspace.com>
2018-12-12 21:44:36 -0400

    d5p1

--- index.js
+++ index.js
@@ -1,103 +1,52 @@
 const fs = require("fs");
-const dateFns = require("date-fns");
+
+let data, current, last, exploder, len;
 
 function readInput() {
   const lineReader = require("readline").createInterface({
     input: require("fs").createReadStream("input.txt")
   });
 
-  let records = [];
-  let emptyArray = [];
-  for (let i = 0; i < 59; i++) {
-    emptyArray[i] = 0;
-  }
-
   lineReader
     .on("line", line => {
-      const match = /\[1518-(\d+)-(\d+) (\d+):(\d+)\]\s(.*)/.exec(line);
-
-      let timestamp = new Date(
-        2018, // because 1518 doesn't matter?
-        parseInt(match[1], 10),
-        parseInt(match[2], 10),
-        parseInt(match[3], 10),
-        parseInt(match[4], 10)
-      );
-
-      records.push({
-        time: timestamp,
-        min: match[4],
-        log: match[5]
-      });
+      data = line;
     })
     .on("close", () => {
-      records = records.sort((a, b) => {
-        if (a.time.getTime() < b.time.getTime()) return -1;
-        if (a.time.getTime() > b.time.getTime()) return 1;
-        return 0;
-      });
+      // this would be easier with regex... right?
+      let done = false;
+      data = data.split("");
+      len = data.length;
 
-      let currentGuard = -1;
-      let sleepStart, sleepEnd;
+      while (!done) {
+        exploder = -1;
+        for (let i = 0; i < len - 1; i++) {
+          // i = 1; i < len; i++ didn't work
+          current = data[i];
+          last = data[i + 1]; // i - 1 didn't work... why not?!?
 
-      let guardLogs = {};
-      let guardTotals = {};
-      records = records.forEach(({ min, log }) => {
-        switch (log) {
-          case "falls asleep":
-            sleepStart = min;
-            break;
-
-          case "wakes up":
-            sleepEnd = min;
-
-            for (let i = sleepStart; i < sleepEnd; i++) {
-              guardLogs[currentGuard][i]++;
-              guardTotals[currentGuard]++;
-            }
-            break;
-
-          default:
-            currentGuard = log.match(/\d+/g);
-            if ("undefined" === typeof guardLogs[currentGuard]) {
-              guardLogs[currentGuard] = [...emptyArray];
-              guardTotals[currentGuard] = 0;
-            }
-
-            break;
-        }
-      });
-
-      const findMaxIndex = arr => {
-        let maxVal = -1,
-          maxIdx = -1;
-        arr.forEach((val, idx) => {
-          if (val > maxVal) {
-            maxVal = val;
-            maxIdx = idx;
+          if (current === undefined) {
+            console.log(data.length);
+            process.exit(0);
           }
-        });
 
-        return [maxIdx, maxVal];
-      };
+          if (
+            (current.toUpperCase() == last && last.toLowerCase() == current) ||
+            (current.toLowerCase() == last && last.toUpperCase() == current)
+          ) {
+            exploder = i;
 
-      let guardHighVal = -1,
-        guardHighIdx = -1,
-        guardHighKey = "";
-      Object.keys(guardLogs).forEach(key => {
-        let [maxIdx, maxVal] = findMaxIndex(guardLogs[key]);
-
-        if (maxVal > guardHighVal) {
-          guardHighVal = maxVal;
-          guardHighIdx = maxIdx;
-          guardHighKey = key;
+            break;
+          }
         }
-        // console.log(key, maxIdx, maxVal);
-      });
 
-      console.log(guardHighIdx, guardHighVal, guardHighKey);
-      console.log(guardHighIdx * guardHighKey);
+        if (exploder < 0) {
+          done = true;
+        } else {
+          data.splice(exploder, 2);
+        }
+      }
 
+      console.log(data.length);
       process.exit(0);
     });
 }
